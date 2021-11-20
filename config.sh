1f04f377fb1bf25731e3d10773221037088c72a8
FConnuli <56132094+FConnuli@users.noreply.github.com>
2021-11-20 01:14:20 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
-sudo pacman -Syu --noconfirm base-devel make xorg xorg-xinit xf86-video-fbdev nitrogen xcompmgr dmenu xmonad xmonad-contrib firefox
+sudo pacman -Syu --noconfirm base-devel make xorg xorg-xinit xf86-video-fbdev nitrogen xcompmgr dmenu xmonad xmonad-contrib firefox xterm
 sudo make clean install -C res/.st-alphaDrac/
 cp -a res/. ~/
